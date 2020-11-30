37e6464d066d2379d049ade7dbd6f0506e3b01be
fuli.wang <fuli.wang@modooplay.com>
2020-11-30 17:00:26 +0800

    origin

---
+++ Assets/Test1/TestLog.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+
+namespace BehaviorDesigner.Runtime.Tasks
+{
+    [TaskDescription("Log is a simple task which will output the specified text and return success. It can be used for debugging.")]
+    [TaskIcon("{SkinColor}LogIcon.png")]
+    public class TestLog : Action
+    {
+        [Tooltip("Text to output to the log")]
+        public SharedString text;
+        public override TaskStatus OnUpdate()
+        {
+            UnityEngine.Debug.LogError(text);
+            return TaskStatus.Success;
+        }
+
+        public override void OnReset()
+        {
+            text = "";
+        }
+
+
+    }
+}
